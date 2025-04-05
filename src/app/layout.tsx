e16587211f0bc058709816373d0ce33a5e52e66a
Moiz Ali Siddiqui <siddiqui.moizali1@gmail.com>
2025-04-05 23:57:15 +0500

    Suppressed the Hydration Warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+
       <head>
         <link rel="icon" href="/favicon.png" /> {/* Updated favicon reference */}
       </head>
